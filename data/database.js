729cb5d4dc484b785e72d3c1195e811b093afd44
Arun V. S <vsdhnanjay16@gmail.com>
2023-08-28 14:30:24 +0530

    deploy commit

--- data/database.js
+++ data/database.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = ()=> {
-    mongoose
-	.connect(process.env.MONGO_URI, {
+    mongoose.connect(process.env.MONGO_URI, {
 		dbName: "backendapi",
-	})
-	.then(console.log("Database Connected"))
-	.catch((e) => console.log(e));
+		useNewUrlParser: true, // Use the new URL parser (prior to Mongoose 6)
+		useUnifiedTopology: true,
+		})
+.then(console.log("Database Connected"))
+.catch((e) => console.log(e));
 };
