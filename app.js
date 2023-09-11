afacf616179237201395761fc33324f8fde1fb4b
Arun V. S <vsdhnanjay16@gmail.com>
2023-09-11 15:55:40 +0530

    changes cors values in app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 import express from "express";
 import userRouter from "./routes/user.js";
 import taskRouter from "./routes/task.js";
-import { config } from "dotenv";import cookieParser from "cookie-parser";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
 import { errorMiddleWare } from "./middlewares/error.js";
 import cors from "cors";
 
 export const app = express();
 
 config({
-	path:"./data/config.env",
-})
+	path: "./data/config.env",
+});
 
 const Router = express.Router();
 
@@ -18,18 +19,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:5173"],
+		origin: [process.env.FRONTEND_URL],
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		credentials: true,
-		exposedHeaders: ["X-Custom-Header"],
 	})
 );
 
-
-
 //using routes
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/task",taskRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
 
 app.get("/", (req, res) => {
 	res.send("noice");
