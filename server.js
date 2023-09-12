3911039c7c6bf8a687584e48ad2f776bb1ebfc7a
Arun V. S <vsdhnanjay16@gmail.com>
2023-09-12 21:35:57 +0530

    try catch in server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import {app} from "./app.js";
 import { connectDB } from "./data/database.js";
 
-connectDB();
+try {
+	connectDB();
+
+} catch (error) {
+	console.log(error);
+}
+
+
 
 app.listen(process.env.PORT, (req, res) => {
 	console.log(`Server is Working on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
